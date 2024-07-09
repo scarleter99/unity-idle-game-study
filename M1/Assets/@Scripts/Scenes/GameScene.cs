f4c95dfec32529873a1e0866888a2564274cb004
Kim Taebum <ddino99@naver.com>
2024-07-09 18:09:33 +0900

    Monster AI

--- M1/Assets/@Scripts/Scenes/GameScene.cs
+++ M1/Assets/@Scripts/Scenes/GameScene.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using static Define;
 
@@ -11,6 +12,23 @@ public class GameScene : BaseScene
 			return false;
 
 		SceneType = EScene.GameScene;
+
+		GameObject map = Managers.Resource.Instantiate("BaseMap");
+		map.transform.position = Vector3.zero;
+		map.name = "@BaseMap";
+
+		Hero hero = Managers.Object.Spawn<Hero>(new Vector3Int(-10, -5, 0));
+		hero.CreatureState = ECreatureState.Move;
+
+		CameraController camera = Camera.main.GetOrAddComponent<CameraController>();
+		camera.Target = hero;
+
+		Managers.UI.ShowBaseUI<UI_Joystick>();
+
+		{
+			Monster monster = Managers.Object.Spawn<Monster>(new Vector3Int(0, 1, 0));
+			monster.CreatureState = ECreatureState.Idle;
+		}
 
 		// TODO
 
